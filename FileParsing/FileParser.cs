961ba00f5548d45f040c851babd0ecf961cca38d
Aleksei Karasev <aleksei.karasev@qoniac.com>
2019-01-04 12:51:11 +0100

    Init commit

---
+++ FileParsing/FileParser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace FileParsing
+{
+    public class FileParser
+    {
+        [Benchmark]
+        public void InMemoryParsing()
+        {
+            ParseFileStream(ftpStream =>
+            {
+                var memoryStream = new MemoryStream();
+
+                var buffer = new byte[16 * 1024];
+                int read;
+                while ((read = ftpStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    memoryStream.Write(buffer, 0, read);
+                }
+
+                memoryStream.Position = 0;
+
+                Parse(memoryStream);
+            });
+        }
+
+        [Benchmark]
+        public void StreamingParsing()
+        {
+            ParseFileStream(Parse);
+        }
+
+        private void ParseFileStream(Action<Stream> parse)
+        {
+            using (var ftpClient = new FluentFtpProvider())
+            {
+                parse(ftpClient.OpenRead("5MB.zip"));
+            }
+        }
+
+        private void Parse(Stream inputStream)
+        {
+            var byteCount = 0;
+
+            const int bufferSize = 5 * 1024 * 1024;
+            var blockHeader = new byte[14];
+            var buffer = new byte[bufferSize];
+
+            bool init = true;
+            int readerStart = 0;
+            int readerLength = buffer.Length;
+            int readByte;
+            while ((readByte = inputStream.Read(buffer, readerStart, readerLength)) > 0)
+            {
+                byteCount += readByte;
+
+                if (init) // set block header
+                {
+                    Array.Copy(buffer, 0, blockHeader, 0, blockHeader.Length);
+                    init = false;
+                }
+
+                int lastPosition = 512;
+
+                var cutSize = lastPosition;
+                var tailSize = buffer.Length - cutSize;
+                Array.Copy(buffer, cutSize, buffer, 0, tailSize);
+                readerStart = tailSize;
+                readerLength = cutSize;
+                // reset search position
+            }
+        }
+    }
+}
---
+++ FileParsing/FluentFtpProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using FluentFTP;
+
+namespace FileParsing
+{
+    /// <summary>
+    /// FtpProvider based on System.Net.FtpClient class for use with Qoniac.Ovalis.DataImport.Core.Net.SimpleFtpClient 
+    /// </summary>
+    internal sealed class FluentFtpProvider : IDisposable
+    {
+        private readonly FtpClient _ftpClient;
+
+        /// <summary>
+        /// Initializes a new instance of NetFtpProvider
+        /// </summary>
+        public FluentFtpProvider()
+        {
+            _ftpClient = new FtpClient
+            {
+                Host = "ftp://speedtest.tele2.net/"
+            };
+        }
+
+        /// <summary>
+        /// Provides a stream to read data from a file on the FTP server
+        /// </summary>
+        /// <param name="path">Path to the file to read</param>
+        /// <returns>Stream to read data</returns>
+        public Stream OpenRead(string path)
+        {
+            return _ftpClient.OpenRead(path);
+        }
+
+        /// <summary>
+        /// Releases the unmanaged resources used by the NetFtpProvider
+        /// </summary>
+        public void Dispose()
+        {
+            _ftpClient.Disconnect();
+            _ftpClient.Dispose();
+        }
+    }
+}
